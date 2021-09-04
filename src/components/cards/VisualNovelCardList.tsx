9b8d3ef63f65331922a8e2185ba3b96c338c81e5
Darren Wihandi <nerradfour@gmail.com>
2021-09-03 22:36:19 -0400

    moved search/fetch implementation to browse page from card list

--- src/components/cards/VisualNovelCardList.tsx
+++ src/components/cards/VisualNovelCardList.tsx
@@ -6,69 +6,28 @@ import useVisualNovelSearch from "../../hooks/useVisualNovelSearch";
 import { VnSearchQuery, VnSearchItem } from "../../vndb/VnTypes";
 import { VndbSearchQuery } from "../../vndb/VndbHelpers";
 
-const VisualNovelCardList = () => {
-  const [lastSortvalue, setLastSortValue] = useState<Number | undefined>(
-    undefined
-  );
+import { Service } from "../../fetch/Service";
 
-  const [lastSortVid, setLastSortVid] = useState<string | undefined>(undefined);
+interface VisualNovelCardListProps {
+  data: Service<VnSearchItem[]>;
+}
 
-  const listParams = {
-    tags: [],
-    sort_by: "popularity",
-    search: undefined,
-    results: 20,
-    last_sort_value: lastSortvalue,
-    last_sort_vid: lastSortVid,
-    nsfw: false,
-  };
-
-  const result = useVisualNovelSearch(listParams);
-
-  const observer = useRef<IntersectionObserver>();
-  const lastItemRef = useCallback(
-    (node) => {
-      if (result.status !== "loaded") return;
-      if (observer.current) observer.current.disconnect();
-      observer.current = new IntersectionObserver((entries) => {
-        if (entries[0].isIntersecting) {
-          const lastItem = result.payload[result.payload.length - 1];
-          let value;
-          if (listParams.sort_by === "popularity") {
-            value = lastItem.popularity;
-            // } else if (listParams.sort_by === "rating") {
-            //   value = lastItem.rating;
-          } else if (listParams.sort_by === "max_released") {
-            value = lastItem.max_released;
-          } else if (listParams.sort_by === "min_released") {
-            value = lastItem.min_released;
-          } else {
-            value = lastItem.max_released;
-          }
-
-          setLastSortValue(value);
-        }
-      });
-      if (node) observer.current.observe(node);
-    },
-    [result]
-  );
-
-  if (result.status === "init") return <div>init</div>;
-  if (result.status === "error")
+const VisualNovelCardList = (props: VisualNovelCardListProps) => {
+  if (props.data.status === "init") return <div>init</div>;
+  if (props.data.status === "error")
     return (
       <div className="flex flex-col justify-start items-center bg-light min-h-screen py-16">
         error
       </div>
     );
-  if (result.status === "loading")
+  if (props.data.status === "loading")
     return (
       <div className="flex flex-col justify-start items-center bg-light min-h-screen">
         loading
       </div>
     );
 
-  const cardList = result.payload.map((vn, idx) => {
+  const cardList = props.data.payload.map((vn) => {
     return <VisualNovelCard vn={vn} key={vn.id} />;
   });
 
@@ -76,7 +35,7 @@ const VisualNovelCardList = () => {
     <>
       <div className="grid grid-cols-normalScreen gap-y-2 justify-between ">
         {cardList}
-        {result.status === "loadingMore" && (
+        {props.data.status === "loadingMore" && (
           <>
             <VisualNovelCardSkeleton />
             <VisualNovelCardSkeleton />
@@ -86,7 +45,6 @@ const VisualNovelCardList = () => {
           </>
         )}
       </div>
-      <div className="w-full h-1" ref={lastItemRef}></div>
     </>
   );
 };
--- src/pages/BrowseVIsualNovelsPage.tsx
+++ src/pages/BrowseVIsualNovelsPage.tsx
@@ -10,6 +10,53 @@ import useVisualNovelSearch from "../hooks/useVisualNovelSearch";
 import * as VNDBHelper from "../vndb/VndbHelpers";
 
 const BrowseVisualNovelsPage = () => {
+  const [lastSortvalue, setLastSortValue] = useState<Number | undefined>(
+    undefined
+  );
+
+  const [lastSortVid, setLastSortVid] = useState<string | undefined>(undefined);
+
+  const listParams = {
+    tags: [],
+    sort_by: "popularity",
+    search: undefined,
+    results: 20,
+    last_sort_value: lastSortvalue,
+    last_sort_vid: lastSortVid,
+    nsfw: false,
+  };
+
+  const result = useVisualNovelSearch(listParams);
+
+  const observer = useRef<IntersectionObserver>();
+  const lastItemRef = useCallback(
+    (node) => {
+      if (result.status !== "loaded") return;
+      if (observer.current) observer.current.disconnect();
+      observer.current = new IntersectionObserver((entries) => {
+        if (entries[0].isIntersecting) {
+          const lastItem = result.payload[result.payload.length - 1];
+          let value;
+          if (listParams.sort_by === "popularity") {
+            value = lastItem.popularity;
+            // } else if (listParams.sort_by === "rating") {
+            //   value = lastItem.rating;
+          } else if (listParams.sort_by === "max_released") {
+            value = lastItem.max_released;
+          } else if (listParams.sort_by === "min_released") {
+            value = lastItem.min_released;
+          } else {
+            value = lastItem.max_released;
+          }
+
+          setLastSortValue(value);
+        }
+      });
+      if (node) observer.current.observe(node);
+    },
+    [result]
+  );
+
   return (
     <div
       className="bg-light w-full min-h-screen bottom-0 px-12
@@ -39,7 +86,8 @@ const BrowseVisualNovelsPage = () => {
             multiSelect={true}
           />
         </div>
-        <VisualNovelCardList />
+        <VisualNovelCardList data={result} />
+        <div className="w-full h-1" ref={lastItemRef}></div>
       </div>
     </div>
   );
