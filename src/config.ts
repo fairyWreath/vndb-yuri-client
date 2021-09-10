1274725a443f5236fa555e8e1029f47270706a7c
Darren Wihandi <nerradfour@gmail.com>
2021-09-10 01:28:52 -0400

    added new api domain

--- src/config.ts
+++ src/config.ts
@@ -2,9 +2,7 @@ import * as dotenv from "dotenv";
 dotenv.config({ path: "../.env" });
 
 const Config = {
-  API_BASE_URL:
-    process.env.API_BASE_URL ||
-    "http://ec2-18-117-174-111.us-east-2.compute.amazonaws.com:3000/",
+  API_BASE_URL: process.env.API_BASE_URL || "https://vnlist-api.xyz/",
 };
 
 export default Config;
