e81f35d376f40af77823e5079c3a52cf0f552722
Darren Wihandi <nerradfour@gmail.com>
2021-09-12 00:53:30 -0400

    fixed https url for api

--- src/config.ts
+++ src/config.ts
@@ -2,7 +2,7 @@ import * as dotenv from "dotenv";
 dotenv.config({ path: "../.env" });
 
 const Config = {
-  API_BASE_URL: "https:vnlist-api.xyz/",
+  API_BASE_URL: "https://vnlist-api.xyz/",
 };
 
 export default Config;
